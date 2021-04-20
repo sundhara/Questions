3630d986db3917daf00e9369b32e50128f87fa59
unknown <yangdl189@189.cn>
2021-04-20 21:14:36 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,35 @@
 <template>
+<el-header></el-header>
+  <el-row :gutter="10">
+    <el-col :xs="8"
+            :sm="6"
+            :md="4"
+            :lg="3"
+            :xl="1">
+      <div class="grid-content bg-purple">1</div>
+    </el-col>
+    <el-col :xs="4"
+            :sm="6"
+            :md="8"
+            :lg="9"
+            :xl="11">
+      <div class="grid-content bg-purple-light"></div>
+    </el-col>
+    <el-col :xs="4"
+            :sm="6"
+            :md="8"
+            :lg="9"
+            :xl="11">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+    <el-col :xs="8"
+            :sm="6"
+            :md="4"
+            :lg="3"
+            :xl="1">
+      <div class="grid-content bg-purple-light"></div>
+    </el-col>
+  </el-row>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
@@ -8,12 +39,12 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      count: 0
+      count: 0,
     }
-  }
+  },
 }
 </script>
